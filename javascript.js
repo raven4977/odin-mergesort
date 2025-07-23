ff2c51a22e633f19ee6b23b138a0922b2f178d9f
Cameron Brown <moontsukuyomi6@gmail.com>
2025-07-23 15:29:50 +0200

    Create a function merge to handle comparing and merging array values

--- javascript.js
+++ javascript.js
@@ -5,7 +5,19 @@ function mergeSort(array) {
   const middle = Math.floor(array.length / 2);
   const leftArray = array.slice(0, middle);
   const rightArray = array.slice(middle);
-  return mergeSort(leftArray), mergeSort(rightArray);
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
+}
+
+function merge(leftArray, rightArray) {
+  const newArray = [];
+  while (leftArray.length && rightArray.length) {
+    if (leftArray[0] <= rightArray[0]) {
+      newArray.push(leftArray.shift());
+    } else {
+      newArray.push(rightArray.shift());
+    }
+  }
+  return [...newArray, ...leftArray, ...rightArray];
 }
 
 console.log(mergeSort([3, 7, 6, 12, 4, 9]));
